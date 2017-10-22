0d3d079fec45fb2802ade9fd4aa9cd99937ac4f0
cad-san <cadsan1986@gmail.com>
2017-10-22 22:40:21 +0900

    implement client.PostBattle()

--- client.go
+++ client.go
@@ -1,10 +1,18 @@
 package statink2
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"path"
+	"runtime"
+	"strings"
 
 	"github.com/pkg/errors"
 )
@@ -17,10 +25,45 @@ type Client struct {
 	Logger *log.Logger
 }
 
+// Battle is battle infomation
+type Battle struct {
+	// rule info
+	Lobby string `json:"lobby"`
+	Mode  string `json:"mode"`
+	Rule  string `json:"rule"`
+
+	// match info
+	Stage      string `json:"stage"`
+	GachiPower int    `json:"estimate_gachi_power"`
+	StartAt    int64  `json:"start_at"`
+	EndAt      int64  `json:"end_at"`
+
+	// result
+	Result       string `json:"result"`
+	KnockOut     string `json:"knock_out"`
+	MyTeamCount  int    `json:"my_team_count"`
+	HisTeamCount int    `json:"his_team_point"`
+
+	// player info
+	Weapon       string `json:"weapon"`
+	RankInTeam   int    `json:"rank_in_team"`
+	KillOrAssist int    `json:"kill_or_assist"`
+	Kill         int    `json:"kill"`
+	Death        int    `json:"death"`
+	Special      int    `json:"special"`
+	Rank         string `json:"rank"`
+	RankExp      int    `json:"rank_exp"`
+	RankAfter    string `json:"rank_after"`
+	RankAfterExp int    `json:"rank_exp_after"`
+}
+
 const (
 	statInkDomainURL = "https://stat.ink"
-	battleURL        = "api/v2/battle"
+	battleAPI        = "api/v2/battle"
+	version          = "0.1"
 )
+
+var userAgent = fmt.Sprintf("stat.ink GoClient/%s (%s", version, runtime.Version())
 
 // NewClient generates Client, http client object for stat.ink
 // It provides a http client with empty cookiejar.
@@ -50,4 +93,59 @@ func newClient(domain, key string, logger *log.Logger) (*Client, error) {
 	}
 
 	return client, nil
+}
+
+// PostBattle send HTTP POST request to stat.ink
+func (c *Client) PostBattle(ctx context.Context, battle Battle) error {
+	json, err := json.Marshal(battle)
+	c.Logger.Printf("request body : %v", string(json))
+	if err != nil {
+		return errors.Wrap(err, "cannot marshal battle struct")
+	}
+	req, err := c.newRequest(ctx, "POST", battleAPI, strings.NewReader(string(json)))
+	if err != nil {
+		return errors.Wrap(err, "cannot create Request for POST")
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.hc.Do(req)
+	if err != nil {
+		return errors.Wrap(err, "http request failed")
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode/100 > 2 {
+		body, _ := ioutil.ReadAll(resp.Body)
+		c.Logger.Printf("error : %s\n", body)
+		return errors.Errorf("request failed. URL : %v, status code:%d)", req.URL, resp.StatusCode)
+	}
+	return nil
+}
+
+func (c *Client) newRequest(ctx context.Context, method, spath string, body io.Reader) (*http.Request, error) {
+	u := *c.URL
+	u.Path = path.Join(c.URL.Path, spath)
+
+	req, err := http.NewRequest(method, u.String(), body)
+	if err != nil {
+		return nil, err
+	}
+	if ctx != nil {
+		req = req.WithContext(ctx)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.APIKey))
+	req.Header.Set("User-Agent", userAgent)
+
+	return req, nil
+}
+
+func decodeBody(resp *http.Response, out interface{}, f *os.File) error {
+	if f != nil {
+		resp.Body = ioutil.NopCloser(io.TeeReader(resp.Body, f))
+		defer f.Close()
+	}
+
+	decoder := json.NewDecoder(resp.Body)
+	return decoder.Decode(out)
 }
--- client_test.go
+++ client_test.go
@@ -1,11 +1,45 @@
 package statink2
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
 
 func TestNewClient(t *testing.T) {
 	dummyKey := "APIKEY"
 	_, err := NewClient(dummyKey, nil)
 	if err != nil {
 		t.Errorf("NewClient() should not be error with %v", err)
+	}
+}
+
+func TestPostBattle(t *testing.T) {
+	dummyKey := "APIKEY"
+
+	muxAPI := http.NewServeMux()
+	muxAPI.HandleFunc("/api/v2/battle", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("", "")
+		strs := strings.Split(r.Header.Get("Authorization"), " ")
+		if strs[1] != dummyKey {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write([]byte(`{"error" : "invalid api key"}`))
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+	testServer := httptest.NewServer(muxAPI)
+	defer testServer.Close()
+
+	c, err := newClient(testServer.URL, dummyKey, nil)
+	if err != nil {
+		t.Errorf("newClient() should not be error with err : %v", err)
+	}
+	b := Battle{}
+	err = c.PostBattle(nil, b)
+	if err != nil {
+		t.Errorf("PostBattle() should not be error with err : %v", err)
 	}
 }
